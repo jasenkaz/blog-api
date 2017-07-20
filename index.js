4ff29d3a4c51767ab62ebb0d95a6e899039ee50b
jasenkaz <jasenkaz@yahoo.com>
2017-07-20 19:11:23 -0400

    commit cors and middleware changes in index.js

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const server = express();
 
 const port = process.env.PORT || 8080;
+const morgan = require('morgan');
+const cors = require('cors');
+
+server.use(morgan('dev'));
+server.use(cors());
 
 
 server.get('/', (req, res) => {
