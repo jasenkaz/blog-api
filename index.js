daa830f2ece78ec26cf714c3cb4f19555ad017b3
jasenkaz <jasenkaz@yahoo.com>
2017-07-20 19:05:46 -0400

    commit again

--- index.js
+++ index.js
@@ -2,7 +2,17 @@ const express = require('express');
 const server = express();
 
 const port = process.env.PORT || 8080;
+//middleware imports
+const morgan = require('morgan');
 
+//wire up the middleware
+server.use(morgan('dev'));
+
+//cors imports
+const cors = require('cors');
+
+//wire up cors
+server.use(cors('dev'));
 
 server.get('/', (req, res) => {
      res.send('it works');
