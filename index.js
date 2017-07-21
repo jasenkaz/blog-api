b270f3980672224c629d1acfbeaaccf747173a80
jasenkaz <jasenkaz@yahoo.com>
2017-07-21 19:52:20 -0400

    made changes to index.js coz i forgot before

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const port = process.env.PORT || 8080;
 const morgan = require('morgan'); //middleware imports
 const cors = require('cors');
 const userRouter = require('./routers/user.router');  //router imports
-
+const postRouter = require('./routers/post.router');
 
 //wire up the middleware
 server.use(morgan('dev'));
 server.use(cors());
 //wire up the routers
 server.use(userRouter);
+server.use(postRouter);
 
 server.get('/', (req, res) => {
      res.send('it works');
