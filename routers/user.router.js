64961c0ff929273cb5bc2b621a3abee48a034143
jasenkaz <jasenkaz@yahoo.com>
2017-07-26 20:23:22 -0400

    added the body parser middleware in order to allow user requests to send additional data Fully implement the CRUD operations throught he HTTP endpoints thus giving moderated access to the database.

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,21 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user.model');
 
 router.get('/users', (req, res) =>{
-  res.send('getting all dem users');
-});
-router.get('/users/:userId', (req, res) =>{
-  res.send('getting that one special someone');
-});
-router.post('/users', (req, res) =>{
-  res.send('creating that new user you requested');
-});
-router.put('/users/:userId', (req, res) =>{
-  res.send('updates errywhere');
-});
-router.delete('/users/:userId', (req, res) =>{
-  res.send('burning it down');
+  //the {} is the query
+    User.find({}, function(err, users){
+      if(err) return res.status(500).json({err: err});
+      return res.status(200).json({
+        users:users
+      })
+    });
+      });
+
+router.get('/users/:userId', (req, res)=>{
+  User.find({ _id : req.params.userId }, function(err, users){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      users:users
+    })
+  });
+    });
+
+router.post('/users', (req, res) => {
+  const newUser = new User(req.body);
+  newUser.save(function(err, user){
+    if(err) return res.status(500).json({err: err});
+    return res.status(201).json({
+      msg: "Succesfully created user"
+    });
+  });
 });
 
+router.put('/users/:userId', (req, res) =>{
+  User.findOneAndUpdate({ _id: req.params.userId}, req.body, function(err){
+    if(err) return res.status(500).json({err: err});
+    return res.status(200).json({
+      msg: "Succesfully updated user"
+    });
+  });
+});
+
+router.delete('/users/:userId', (req, res) =>{
+  User.findOneAndRemove({ _id: req.params.userId}, function(err, removedUser){
+    if(err) return res.status(500).json({err: err}); //look up http status codes in google
+    return res.status(200).json({
+      msg: "User succesfully removed"
+    })
+  });
+});
 
 module.exports = router;
