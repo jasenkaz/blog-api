2a05d9a002c4876e99011fe8c8e2b0a801b05bc2
jasenkaz <jasenkaz@yahoo.com>
2017-08-01 11:22:16 -0400

    turned on cors with middleware

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const server = express();
 
-const { mongoURI } = require('./credentials'); //new syntax; handy but not necessary
-// const mongoURI = require('./credentials').mongoURI; //old style, still works
 const port = process.env.PORT || 8080;
-
-//connect to the database
-mongoose.connect(mongoURI, {
-  useMongoClient: true
-});
 
 //middleware imports
 const morgan = require('morgan');
 const cors = require('cors');
-const bodyParser = require('body-parser');
-
-//router imports
-const userRouter = require('./routers/user.router');
-const postRouter = require('./routers/post.router');
 
 //wire up the middleware
 server.use(cors());
 server.use(morgan('dev'));
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
-
-//wire up the routers
-server.use(userRouter);
-server.use(postRouter);
 
 
 server.get('/', (req, res) => {
