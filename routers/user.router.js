1ebce29fc304e8721a7302ceebd563ad636abe17
jasenkaz <jasenkaz@yahoo.com>
2017-08-02 18:38:43 -0400

    changed index.js

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const express = require('express');
-const router = express.Router();
 const User = require('../models/user.model');
 
 router.get('/users', (req, res) =>{
